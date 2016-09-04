9b4edb9afbeff51d9730b3fbd29847e0a8083a1e
XhmikosR <xhmikosr@gmail.com>
2016-09-04 11:57:41 +0300

    Move GA track events to main.js.

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,14 +2,14 @@
     'use strict';
 
     (function () {
-        var el = document.querySelectorAll('.input-group-btn > button');
+        var el = window.document.querySelectorAll('.input-group-btn > button');
 
         function toggleCode (index) {
             var name = el[index].getAttribute('data-el');
             var btnIcon = el[index].querySelector('span');
 
             el[index].addEventListener('click', function() {
-                document.getElementById(name).classList.toggle('hidden');
+                window.document.getElementById(name).classList.toggle('hidden');
                 btnIcon.classList.toggle('caret-open');
             });
         }
@@ -22,7 +22,7 @@
     })();
 
     (function () {
-        var el = document.querySelectorAll('input[type="text"');
+        var el = window.document.querySelectorAll('input[type="text"');
 
         if (el) {
             for (var i = 0, len = el.length; i < len; i++) {
@@ -36,8 +36,36 @@
         }
     })();
 
-    /* eslint func-style: 0 */
+
+    (function () {
+        function gaEvent(e) {
+            if (typeof e.target !== 'undefined') {
+                var action = e.target.getAttribute('data-ga-action');
+                var category = e.target.getAttribute('data-ga-category');
+                var label = e.target.getAttribute('data-ga-label');
+                var value = parseInt(e.target.getAttribute('data-ga-value'), 10);
+
+                if (typeof ga !== 'undefined' && typeof category !== 'undefined' && typeof action !== 'undefined') {
+                    window.ga('send', 'event', category, action, label, value, {});
+                }
+            }
+        }
+
+        /* eslint-disable */
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-32253110-1', 'bootstrapcdn.com');
+        ga('send', 'pageview');
+        window.document.addEventListener('click', gaEvent);
+        /* eslint-enable */
+    })();
+
+
     (function(win, doc) {
+        /* eslint func-style: 0 */
         var init = function() {
             var script = doc.createElement('script');
 
@@ -76,15 +104,6 @@
 
         return t;
     }(document, "script", "twitter-wjs"));
-
-
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-32253110-1', 'bootstrapcdn.com');
-    ga('send', 'pageview');
     /* eslint-enable */
 
 })();
